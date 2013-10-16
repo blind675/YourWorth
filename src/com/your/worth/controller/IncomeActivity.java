97c6887f364457328862b9dc993358649c9de06c
Catalin <catalin.bora@gmail.com>
2013-10-16 21:00:52 +0300

    Finished the income controller. (For now)

--- src/com/your/worth/controller/IncomeActivity.java
+++ src/com/your/worth/controller/IncomeActivity.java
@@ -65,11 +65,9 @@ public class IncomeActivity extends Activity {
             }
         }
 
-
-
-
         ArrayAdapter<String> adapter = new IncomeAdapter(this,list);
 
+        //TODO: examine
         // do i need to erase the content of the List View first ?
         // Nop.. this seams to be the default way to work with adapters ListView
 
--- src/com/your/worth/controller/IncomeAdapter.java
+++ src/com/your/worth/controller/IncomeAdapter.java
@@ -23,7 +23,9 @@ public class IncomeAdapter extends ArrayAdapter<String> {
     private final Context mContext;
     private final ArrayList<String> mValues;
     private final LayoutInflater mInflated;
-    
+    private ImageView mImageView;
+    private TextView mTextView;
+
     // create a constructor for my custom adapter
     public IncomeAdapter(Context context, ArrayList<String> values) {
         super(context, R.layout.row, values);
@@ -36,24 +38,23 @@ public class IncomeAdapter extends ArrayAdapter<String> {
     @Override
     public View getView(final int position, View convertView, final ViewGroup parent) {
         // inflate the layout XML only if the convertView is null
-        // the version sugested by google
-        if(convertView == null ) {
+        // the version suggested by google
+        if (convertView == null){
             convertView = mInflated.inflate(R.layout.row, parent, false);
         }
-        
-        TextView textView = (TextView) convertView.findViewById(android.R.id.text1);
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.imgIcon);
-        textView.setText(mValues.get(position).toString());
-        
+
+        mTextView = (TextView) convertView.findViewById(android.R.id.text1);
+        mImageView = (ImageView) convertView.findViewById(R.id.imgIcon);
+        mTextView.setText(mValues.get(position));
+
         // remove the row if the x icon was clicked
-        imageView.setOnClickListener( new View.OnClickListener() {
+        mImageView.setOnClickListener( new View.OnClickListener() {
 
             public void onClick(View v) {
                 mValues.remove(position);
                 AppModel.getInstance().removeIncomeRecord(position);
                 
-                //TODO: notify the adapter that a refresh is needed .. don't yet know how
-                // notifyDataSetChanged()
+                notifyDataSetChanged();
             }
         });
 
